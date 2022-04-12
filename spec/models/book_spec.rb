99b770980cf2c953029e049483d4b4c86e69bbb2
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-12 19:52:57 +0100

    First Commit

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  # Association test
+  # Association test
+  it { should belong_to(:category) }
+  # Validation tests
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:author) }
+  it { should validate_length_of(:title).is_at_least(3) }
+end
